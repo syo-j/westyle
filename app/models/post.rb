ceb9504a5624c4aa54feb221bd1efeff7d0178bc
syo-j <syouj@icloud.com>
2020-04-01 18:13:03 +0900

    defalut.img

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,15 +2,15 @@ class Post < ApplicationRecord
 	
 	belongs_to :user, optional: true
 	
-	has_many :likes
+	has_many :likes, dependent: :destroy
 	# 投稿が誰にいいねされているのか
-	has_many :liked_users, through: :likes, source: :user
+	has_many :liked_users, through: :likes, source: :user, dependent: :destroy
 	
 	has_many :comments, dependent: :destroy
 	# コメントが存在するかを見るため
 	has_many :commented_users, through: :comments, source: :user
 
-	has_many :clothes, inverse_of: :post
+	has_many :clothes, inverse_of: :post, dependent: :destroy
 	#postのフォームでclotheを保存
 	accepts_nested_attributes_for :clothes, allow_destroy: true
 
