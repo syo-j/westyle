57534307eae5a38f4917c33980ed103360390327
syo-j <syouj@icloud.com>
2020-04-22 13:52:15 +0900

    like ajax

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,19 @@
 class LikesController < ApplicationController
 
+	before_action :set_variables
+
 	def create
 		@like = current_user.likes.create(post_id: params[:post_id])
-    	redirect_back(fallback_location: root_path)
+    	# redirect_back(fallback_location: root_path)
 	end
 
 	def destroy
 		@like = Like.find_by(post_id: params[:post_id], user_id: current_user.id)
     	@like.destroy
-    	redirect_back(fallback_location: root_path)
+    	# redirect_back(fallback_location: root_path)
+	end
+
+	def set_variables
+		@post = Post.find(params[:post_id])
 	end
 end
