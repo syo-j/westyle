61010fa999e5e35950c37c651809cc97c167426a
syo-j <syouj@icloud.com>
2020-04-28 12:07:09 +0900

    conhig update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,19 +62,19 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = {host: 'http://westyle.herokuapp.com' }
-    config.action_mailer.raise_delivery_errors = false　#この一文も追記!!
-    config.action_mailer.delivery_method = :smtp
-    config.action_mailer.smtp_settings = {
-        :user_name => ENV['SENDGRID_USERNAME'],
-        :password => ENV['SENDGRID_PASSWORD'],
-        :domain => "heroku.com",
-        :address => "smtp.sendgrid.net",
-        :port => 587,
-        :authentication => :plain,
-        :enable_starttls_auto => true
-    }
-  end
+  # config.action_mailer.default_url_options = {host: 'http://westyle.herokuapp.com' }
+  #   config.action_mailer.raise_delivery_errors = false　#この一文も追記!!
+  #   config.action_mailer.delivery_method = :smtp
+  #   config.action_mailer.smtp_settings = {
+  #       :user_name => ENV['SENDGRID_USERNAME'],
+  #       :password => ENV['SENDGRID_PASSWORD'],
+  #       :domain => "heroku.com",
+  #       :address => "smtp.sendgrid.net",
+  #       :port => 587,
+  #       :authentication => :plain,
+  #       :enable_starttls_auto => true
+  #   }
+  # end
 
 
   # Ignore bad email addresses and do not raise email delivery errors.
