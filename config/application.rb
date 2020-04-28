dd802bd88d5d156d1c82b9e6e396bd53b6930e30
syo-j <syouj@icloud.com>
2020-04-28 11:56:46 +0900

    mail

--- config/application.rb
+++ config/application.rb
@@ -20,6 +20,9 @@ module Westyle
     #erd
     config.autoloader = :classic
 
+    # メール
+    config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,6 +62,21 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = {host: 'http://westyle.herokuapp.com' }
+    config.action_mailer.raise_delivery_errors = false　#この一文も追記!!
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+        :user_name => ENV['SENDGRID_USERNAME'],
+        :password => ENV['SENDGRID_PASSWORD'],
+        :domain => "heroku.com",
+        :address => "smtp.sendgrid.net",
+        :port => 587,
+        :authentication => :plain,
+        :enable_starttls_auto => true
+    }
+  end
+
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
