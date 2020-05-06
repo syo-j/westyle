5a6a38714cc10ec64a5d142531920a4466b6ab27
syo-j <syouj@icloud.com>
2020-05-06 12:44:13 +0900

    new video form add

--- app/assets/javascripts/img_prev.js
+++ app/assets/javascripts/img_prev.js
@@ -22,3 +22,29 @@ $(function(){
     reader.readAsDataURL(file);
   });
 });
+
+// video
+$(function(){
+  $fileField = $('#videofile')
+ 
+  // 選択された画像を取得し表示
+  $($fileField).on('change', $fileField, function(e) {
+    file = e.target.files[0]
+    reader = new FileReader()
+    $preview = $("#video_field");
+ 
+    reader.onload = (function(file) {
+      return function(e) {
+        $preview.empty();
+        $preview.append($('<video>').attr({
+          src: e.target.result,
+          width: "100%",
+          class: "preview",
+          title: file.name
+        }));
+      };
+    })(file);
+    reader.readAsDataURL(file);
+  });
+});
+
